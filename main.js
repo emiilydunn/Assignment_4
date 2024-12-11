c154af549e9a81c54d88012a8b252daa698d9106
Emily Dunn <emilydunn13@gmail.com>
2024-12-11 16:35:42 -0400

    api code

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 // IIFE
 (() => {
 
+//Req 1: Retrieval of the JSON starting data
+//Define API url
+var apiURL = "https://prog2700.onrender.com/threeinarow/sample";
+
+
+function fetchPuzzleData(){
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => {
+            //Variables to store the puzzle and solution data
+            createPuzzleGrid(data.rows);
+        })      
+}
+
+
+//Req 2: Draw and display 3-in-a-row table with JS only
+//Function to create the puzzle grid (unobtrusive JS)
+
+//Req 3: Changing of aquare colours with mouse clicks
+
+//Req 4: 3-in-a-row puzzle status checking
+
+//Req 5: Error display checkbox
+
+//Req 6: Adding innovative feature 
+
 
 
 })();
