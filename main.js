7b5601fb31af7028ce74781d8df36b1f2940ebe9
Emily Dunn <emilydunn13@gmail.com>
2024-12-12 00:16:27 -0400

    added random link for testing

--- main.js
+++ main.js
@@ -2,8 +2,12 @@
 (() => {
 
     //Define API URL and initialize variable to hold puzzle data
-    const apiURL = "https://prog2700.onrender.com/threeinarow/sample";
+    //const apiURL = "https://prog2700.onrender.com/threeinarow/sample";
     let puzzleData = {};
+
+    //Random link for testing
+    //https://prog2700.onrender.com/threeinarow/random	
+    
 
     //Initialize timer variables
     let startTime;
